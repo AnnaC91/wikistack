e8f70443014fb5b3d89ddec77520d6e900706163
Mary Yen <mymaryyn@gmail.com>
2017-08-07 11:11:49 -0400

    older pg version installed

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
 app.use(bodyParser.json()); // would be for AJAX requests
 
 //syncing db
-models.db.sync({})
+models.db.sync({force: true})
 .then(function () {
     return models.Page.sync({})
 })
